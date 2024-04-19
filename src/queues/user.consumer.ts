097b7f7e12673db2a236b71c51d220b0f7cb1a4e
payam <payamsiami90@gmail.com>
2024-04-18 22:22:05 -0700

    add user elastic

---
+++ src/queues/user.consumer.ts
@@ -0,0 +1,144 @@
+import { config } from '@jobber/config';
+import { IBuyerDocument, ISellerDocument, winstonLogger } from '@jobber/shared';
+import { Channel, ConsumeMessage, Replies } from 'amqplib';
+import { Logger } from 'winston';
+import { createConnection } from '@jobber/queues/connection';
+import { buyerService } from '@jobber/services/buyer.service';
+import { sellerService } from '@jobber/services/seller.service';
+import { publishDirectMessage } from '@jobber/queues/user.producer';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'usersServiceConsumer', 'debug');
+
+const consumeBuyerDirectMessage = async (channel: Channel): Promise<void> => {
+  try {
+    if (!channel) {
+      channel = (await createConnection()) as Channel;
+    }
+    const exchangeName = 'jobber-buyer-update';
+    const routingKey = 'user-buyer';
+    const queueName = 'user-buyer-queue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+    await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey);
+    channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+      const { type } = JSON.parse(msg!.content.toString());
+      if (type === 'auth') {
+        const { username, email, profilePicture, country, createdAt } = JSON.parse(msg!.content.toString());
+        const buyer: IBuyerDocument = {
+          username,
+          email,
+          profilePicture,
+          country,
+          purchasedGigs: [],
+          createdAt
+        };
+        await buyerService.createBuyer(buyer);
+      } else {
+        const { buyerId, purchasedGigs } = JSON.parse(msg!.content.toString());
+        await buyerService.updateBuyerPurchasedGigsProp(buyerId, purchasedGigs, type);
+      }
+      channel.ack(msg!);
+    });
+  } catch (error) {
+    log.log('error', 'UsersService UserConsumer consumeBuyerDirectMessage() method error:', error);
+  }
+};
+
+const consumeSellerDirectMessage = async (channel: Channel): Promise<void> => {
+  try {
+    if (!channel) {
+      channel = (await createConnection()) as Channel;
+    }
+    const exchangeName = 'jobber-seller-update';
+    const routingKey = 'user-seller';
+    const queueName = 'user-seller-queue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+    await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey);
+    channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+      const { type, sellerId, ongoingJobs, completedJobs, totalEarnings, recentDelivery, gigSellerId, count } = JSON.parse(
+        msg!.content.toString()
+      );
+      if (type === 'create-order') {
+        await sellerService.updateSellerOngoingJobsProp(sellerId, ongoingJobs);
+      } else if (type === 'approve-order') {
+        await sellerService.updateSellerCompletedJobsProp({
+          sellerId,
+          ongoingJobs,
+          completedJobs,
+          totalEarnings,
+          recentDelivery
+        });
+      } else if (type === 'update-gig-count') {
+        await sellerService.updateTotalGigsCount(`${gigSellerId}`, count);
+      } else if (type === 'cancel-order') {
+        await sellerService.updateSellerCancelledJobsProp(sellerId);
+      }
+      channel.ack(msg!);
+    });
+  } catch (error) {
+    log.log('error', 'UsersService UserConsumer consumeSellerDirectMessage() method error:', error);
+  }
+};
+
+const consumeReviewFautMessages = async (channel: Channel): Promise<void> => {
+  try {
+    if (!channel) {
+      channel = (await createConnection()) as Channel;
+    }
+    const exchangeName = 'jobber-review';
+    const queueName = 'seller-review-queue';
+    await channel.assertExchange(exchangeName, 'fanout');
+    const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+    await channel.bindQueue(jobberQueue.queue, exchangeName, '');
+    channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+      const { type } = JSON.parse(msg!.content.toString());
+      if (type === 'buyer-review') {
+        await sellerService.updateSellerReview(JSON.parse(msg!.content.toString()));
+        await publishDirectMessage(
+          channel,
+          'jobber-update-gig',
+          'update-gig',
+          JSON.stringify({ type: 'updateGig', gigReview: msg!.content.toString() }),
+          'Message sent to gig service.'
+        );
+      }
+      channel.ack(msg!);
+    });
+  } catch (error) {
+    log.log('error', 'UsersService UserConsumer consumeReviewFautMessages() method error:', error);
+  }
+};
+
+const consumeSeedGigDirectMessages = async (channel: Channel): Promise<void> => {
+  try {
+    if (!channel) {
+      channel = (await createConnection()) as Channel;
+    }
+    const exchangeName = 'jobber-gig';
+    const routingKey = 'get-sellers';
+    const queueName = 'user-gig-queue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const jobberQueue: Replies.AssertQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+    await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey);
+    channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+      const { type } = JSON.parse(msg!.content.toString());
+      if (type === 'getSellers') {
+        const { count } = JSON.parse(msg!.content.toString());
+        const sellers: ISellerDocument[] = await sellerService.getRandomSellers(parseInt(count, 10));
+        await publishDirectMessage(
+          channel,
+          'jobber-seed-gig',
+          'receive-sellers',
+          JSON.stringify({ type: 'receiveSellers', sellers, count }),
+          'Message sent to gig service.'
+        );
+      }
+      channel.ack(msg!);
+    });
+  } catch (error) {
+    log.log('error', 'UsersService UserConsumer consumeReviewFautMessages() method error:', error);
+  }
+};
+
+export { consumeBuyerDirectMessage, consumeSellerDirectMessage, consumeReviewFautMessages, consumeSeedGigDirectMessages };
