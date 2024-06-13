098f6446c21868cc2c6a4ff8d18b22839dba21af
payam <payamsiami90@gmail.com>
2024-06-13 05:33:50 -0700

    update

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,6 @@ class Config {
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
   public GATEWAY_JWT_TOKEN: string | undefined;
-  public API_GATEWAY_URL: string | undefined;
   public REDIS_HOST: string | undefined;
   public MESSAGE_BASE_URL: string | undefined;
   public STRIPE_API_KEY: string | undefined;
@@ -76,7 +75,6 @@ class Config {
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
-    this.API_GATEWAY_URL = process.env.API_GATEWAY_URL || '';
     this.STRIPE_API_KEY = process.env.STRIPE_API_KEY || '';
   }
 
